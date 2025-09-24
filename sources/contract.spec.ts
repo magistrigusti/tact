49a16903040f75ef5a2e09b534979e107d2c5f28
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-09-24 11:55:31 +0300

    0.1.3

--- sources/contract.spec.ts
+++ sources/contract.spec.ts
@@ -1,4 +1,21 @@
+import { toNano } from "@ton/core";
+import { Blockchain } from "@ton/sandbox";
+import "@ton/test-utils";
+import { BroadcastContract } from "./output/sample_BroadcastContract";
 
+describe("contract", () => {
+  it("should deploy correctly", async () => {
+    const system = await Blockchain.create();
+    const owner = await system.treasury("owner");
+    const user1 = await system.treasury("user1");
+    const user2 = await system.treasury("user2");
+    const broadcast = system.openContract(await BroadcastContract.fromInit(owner.address, 0n));
+    await broadcast.send(owner, {value: toNano(1)}, {$$type: 'AddMessage', address: user1.address});
+    await broadcast.send(owner, {value: toNano(1)}, {$$type: "AddMessage", address: user2.address});
+    let tracker = system.track(broadcast.address);
+    await broadcast.send(owner, {value: toNano(5)}, {$$type: "BroadcastMesage", message: 'hi'});
+  })
+})
 
 
 // import { toNano } from "@ton/core";
@@ -12,7 +29,7 @@
 //         const system = await Blockchain.create();
 //         const owner = await system.treasury("owner");
 //         const nonOwner = await system.treasury("non-owner");
-//         const contract = system.openContract(await SampleTactContract.fromInit(owner.address, 0n));
+//const contract = system.openContract(await SampleTactContract.fromInit(owner.address, 0n));
 //         const deployResult = await contract.send(owner.getSender(), { value: toNano(1) }, null);
 //         expect(deployResult.transactions).toHaveTransaction({
 //             from: owner.address,
